14b1a95fb7f779e267dd458c6c48a62368cd6b89
Feng Yu <fyu@ok988.com>
2013-12-02 18:14:31 +0800

    修改py脚本(未完成)

--- playbaidumusic.py
+++ playbaidumusic.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 #coding:utf8
 #python脚本，利用百度音乐API直接播放百度音乐
 
 import httplib2
-from xml.etree import ElementTree as ET
+from xml.dom import minidom
 import os
 
 def getmp3url(title='安静',performer='周杰伦'):
     urlrequest = httplib2.Http()
     xml=urlrequest.request('http://box.zhangmen.baidu.com/x?op=12&count=1&title={0}$${1}$$$$'.format(title,performer))[1]
-    xml_parse = ET.fromstring(xml)
-    count = xml_parse.findall('count')
-    print(count)
+    xml_parse = minidom.parseString(xml.encode('utf-8'))
+    count = xml_parse.getElementsByTagName('count')[0]
+    print count.childNodes[0].data
     
-
 if __name__ == '__main__':
     getmp3url()
